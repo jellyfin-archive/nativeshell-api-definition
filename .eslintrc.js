1121da4c11a95e81e58eeeac35e9b8947d58a054
Cromefire_ <cromefire_@outlook.com>
2020-04-15 13:14:03 +0200

    Added initial structure

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    quotes: ["error", "double"],
+  },
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# Jellyfin NativeShell API definition
+[![pipeline status](https://gitlab.com/cromefire_/nativeshell-api-definition/badges/master/pipeline.svg)](https://gitlab.com/cromefire_/nativeshell-api-definition/-/commits/master)
